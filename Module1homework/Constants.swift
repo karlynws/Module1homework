64c0f2f666929caa93c05b52265fad3da9ba238d
karlynws <146772646+karlynws@users.noreply.github.com>
2024-01-04 12:43:46 -0700

    week 2 work

---
+++ Module1homework/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Module1homework
+//
+//  Created by Karlyn Sanders on 1/3/24.
+//
+
+import Foundation
---
+++ Module1homework/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  Module1homework
+//
+//  Created by Karlyn Sanders on 1/2/24.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
